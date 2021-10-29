7d5cb8e06cddd8046fe1ce768c4cc7eb4dca8704
Filatov_Yurii <filatov.ios.dev@gmail.com>
2021-10-29 16:16:48 +0300

    Drowing 1

---
+++ drawing/drawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  drawingApp.swift
+//  drawing
+//
+//  Created by Юрий Филатов on 29.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct drawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
